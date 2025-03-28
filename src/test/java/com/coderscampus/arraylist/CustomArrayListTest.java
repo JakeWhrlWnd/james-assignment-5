c09a9e933418e4a0654427b4ba55faba8542c327
James <justjamescreative@outlook.com>
2025-03-28 00:09:39 -0400

    Created CustomArrayListTest
    Following along with Trevor in the first video from Unit 11

---
+++ src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -0,0 +1,23 @@
+package com.coderscampus.arraylist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomArrayListTest {
+
+    @Test
+    void add() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void getSize() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    void get() {
+        fail("Not yet implemented");
+    }
+}
