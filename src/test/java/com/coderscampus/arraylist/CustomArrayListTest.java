24f11fc1ef99b4f70f2ae7bf7090b30b123e2a36
James <justjamescreative@outlook.com>
2025-03-28 00:37:26 -0400

    Updated the CustomArrayListTest and the CustomArrayList files
    Within the test, I added some information from the video that I watched. There is some notation that shows the steps of Test-Driven Methodology and the 3-A's. Then I implemented the 3-A's.
    
    In the CustomArrayList, I annotated the get() to suppress warnings.

--- src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
+++ src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -6,9 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CustomArrayListTest {
 
+    // Test-Driven Development Methodology
+    // Step 1 - Write a failing test
+    // Step 2 - Write the business logic to make the test pass
+    // Step 3 - Refactor your code
+    // Rinse and repeat Steps 1 - 3
+
+    // Three A's - "ARRANGE, ACT, ASSERT"
+    // Arrange - set things up
+    // Act - calling the method
+    // Assert - expected behavior/actual behavior
+
     @Test
     void add() {
-        fail("Not yet implemented");
+        // Arrange
+        CustomList<Integer> customList = new CustomArrayList<>();
+        // Act
+        customList.add(10);
+        // Assert
+        // Expectation - There should be an Integer w/ value 10 stored in the first index of customList
+        assertEquals(10, customList.get(0));
+        assertEquals(1, customList.getSize());
     }
 
     @Test
