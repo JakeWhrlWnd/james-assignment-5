ae441676f0172da1726d5e4dc43eb61281c2432a
James <justjamescreative@outlook.com>
2025-03-28 16:57:51 -0400

    Updated the CustomArrayListTest file
    Added more tests to test the effectiveness of my current add() method

--- src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
+++ src/test/java/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -20,13 +20,44 @@ class CustomArrayListTest {
     @Test
     void add() {
         // Arrange
-        CustomList<Integer> customList = new CustomArrayList<>();
+        CustomList<Integer> sut = new CustomArrayList<>();
         // Act
-        customList.add(10);
+        sut.add(10);
+        Integer expectedResult = sut.get(0);
+        Integer expectedSize = sut.getSize();
         // Assert
         // Expectation - There should be an Integer w/ value 10 stored in the first index of customList
-        assertEquals(10, customList.get(0));
-        assertEquals(1, customList.getSize());
+        assertEquals(10, expectedResult);
+        assertEquals(1, expectedSize);
+    }
+
+    @Test
+    void should_add_11_items_to_list() {
+        // Arrange
+        CustomList<Integer> sut = new CustomArrayList<>(); // System Under Test - used when testing
+        // Act
+        for (int i = 1; i <= 100000; i++) {
+            sut.add(i);
+        }
+//        for (int i = 1; i < 22; i++) {
+//            sut.add(i);
+//        }
+//        sut.add(1); sut.add(2); sut.add(3); sut.add(4);
+//        sut.add(5); sut.add(6); sut.add(7); sut.add(8);
+//        sut.add(9); sut.add(10); sut.add(11);
+        // Assert
+//        for (int i = 0; i < 10; i++) {
+//            assertEquals(i + 1, sut.get(i));
+//        }
+//        assertEquals(11, sut.getSize());
+//        for (int i = 0; i < 21; i++) {
+//            assertEquals(i + 1, sut.get(i));
+//        }
+//        assertEquals(21, sut.getSize());
+        for (int i = 0; i < 99999; i++) {
+            assertEquals(i + 1, sut.get(i));
+        }
+        assertEquals(100000, sut.getSize());
     }
 
     @Test
